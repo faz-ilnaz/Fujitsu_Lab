53f69c980ae24394a78d6374b222b11c334914ef
Ilnaz Fazliakhmetov <ilnaz94@gmail.com>
2013-10-27 18:08:27 +0400

    editing NamePublisher.java

--- NamePublisher.java
+++ NamePublisher.java
@@ -2,6 +2,7 @@ public class NamePublisher {
 	public static void main(String[] args) {
 		String[] nameList = {	"John", 
 								"Alex",
-								"Michael"};
+								"Michael",
+								"Ilnaz"};
 	}
 }
